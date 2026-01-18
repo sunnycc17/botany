e847442961310fada2e7c217312dd5381d56abfa
unknown <alkristy7@gmail.com>
2026-01-18 15:06:21 +0100

    added fonts and redigned hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Cinzel, Cormorant } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+export const cinzel = Cinzel({
   subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-cinzel",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+export const cormorant = Cormorant({
   subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-cormorant",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}  antialiased`}
+        className={`${cinzel.variable} ${cormorant.variable}  antialiased`}
       >
         {children}
       </body>
